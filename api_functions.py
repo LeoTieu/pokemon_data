87cc1a8fc0edeee1d98ce4cb1a073b52bc592320
Mayo Layo <l.t05@outlook.com>
2025-06-18 15:43:18 +0200

    Added function for getting pokemon types

--- api_functions.py
+++ api_functions.py
@@ -1,11 +1,11 @@
 import requests
 import json
 
-base_url = "https://pokeapi.co/api/v2/"
+BASE_URL = "https://pokeapi.co/api/v2/"
 
 
 def get_pokemon_info(pokemon: str) -> json:
-    pokemon_url = f"{base_url}pokemon/{pokemon}"
+    pokemon_url = f"{BASE_URL}pokemon/{pokemon}"
     response = requests.get(pokemon_url)
 
     if response.status_code != 200:
@@ -16,8 +16,9 @@ def get_pokemon_info(pokemon: str) -> json:
 
 
 def get_pokemon_names() -> list:
-    current_pokemons = 1025
-    url = f"{base_url}pokemon?limit={current_pokemons}&offset=0"
+    CURRENT_POKEMONS_AMOUNT = 1025
+    
+    url = f"{BASE_URL}pokemon?limit={CURRENT_POKEMONS_AMOUNT}&offset=0"
     response = requests.get(url)
 
     if response.status_code != 200:
@@ -29,7 +30,34 @@ def get_pokemon_names() -> list:
     return pokemon_list
 
 
+def get_pokemon_types(format: str = "dict") -> dict | list:
+    url = f"{BASE_URL}type/"
+    print(url)
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        print(f"Error code given: {response.status_code}")
+        return
+
+    # Gets the all types in a list of dicts.
+    list_of_dicts = response.json()["results"]
+
+    if format == "dict":
+        type_dict = {}
+        for sub_dict in list_of_dicts:
+            pokemon_type = sub_dict["name"]
+            type_url = sub_dict["url"]
+            type_dict[pokemon_type] = type_url
+        return type_dict
+    elif format == "list":
+        type_list = [dict['name'] for dict in list_of_dicts]
+        return type_list
+    else:
+        print(f"Format: {format} is not supported. Use 'list' or 'dict'")
+
+    
 if __name__ == '__main__':
     # print(get_pokemon_names())
     # print(get_pokemon_info("pikachu"))
+    # print(get_pokemon_types())
     pass

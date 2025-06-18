b11d53543e388abd886b8b06e9cadec8ad527d5d
Mayo Layo <l.t05@outlook.com>
2025-06-19 01:33:21 +0200

    Made all possible combinations, won't be that much of a difference in size anyways.

--- misc.py
+++ misc.py
@@ -4,7 +4,7 @@ from api_functions import get_pokemon_names, get_pokemon_info, get_pokemon_types
 def get_all_current_type_combinations():
     '''
     Running takes a lot of time because of 1300+ api calls.
-    Don't run if you don't have to. Thanks!
+    "Don't run if you don't have to. Thanks!
     '''
     user_input = input("Are you sure you want to run? Y/N")
     if  user_input.lower != "y":
@@ -21,10 +21,20 @@ def get_all_current_type_combinations():
         type_combination = tuple(sorted(type_combination))
         pokemon_set.add(type_combination)
         counter += 1
-    print(pokemon_set)
-    print("done")
+    return(pokemon_set)
 
 
-    
+def get_all_possible_type_combinations() -> list:
+    type_list = get_pokemon_types()
+    # Not a real type, issue with api. 
+    type_list.remove("unknown")
 
-get_all_current_type_combinations()
+    all_possible = []
+    while len(type_list) != 0:
+        current = type_list.pop()
+        for type in type_list:
+            all_possible.append(sorted([current, type]))
+    return all_possible
+
+# get_all_current_type_combinations()
+print(len(get_all_possible_type_combinations()))

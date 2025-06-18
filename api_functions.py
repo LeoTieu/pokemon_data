dfd946fe354852b8444d7bbd7b98755c5c8e7e16
Mayo Layo <l.t05@outlook.com>
2025-06-18 15:49:01 +0200

    Added docstrings

--- api_functions.py
+++ api_functions.py
@@ -4,7 +4,11 @@ import json
 BASE_URL = "https://pokeapi.co/api/v2/"
 
 
-def get_pokemon_info(pokemon: str) -> json:
+def get_pokemon_info(pokemon: str) -> dict:
+    '''
+    Get a lot of information of a specific pokemon.
+    Very messy and currently hard to read the return.
+    '''
     pokemon_url = f"{BASE_URL}pokemon/{pokemon}"
     response = requests.get(pokemon_url)
 
@@ -16,6 +20,11 @@ def get_pokemon_info(pokemon: str) -> json:
 
 
 def get_pokemon_names() -> list:
+    '''
+    Returns a list of all current pokemon names. 
+    Must manually enter current pokemon amount manually. 
+    Current pokemon amount also works as a limit of how many pokemons to get.
+    '''
     CURRENT_POKEMONS_AMOUNT = 1025
     
     url = f"{BASE_URL}pokemon?limit={CURRENT_POKEMONS_AMOUNT}&offset=0"
@@ -31,6 +40,13 @@ def get_pokemon_names() -> list:
 
 
 def get_pokemon_types(format: str = "dict") -> dict | list:
+    '''
+    Gets all current pokemon types and returns them in either a dict or a list.
+    Dict format has key = type and value = api type link.
+    List just has all current pokemon types.
+
+    Pokémon type 'unknown' exists for some reason.
+    '''
     url = f"{BASE_URL}type/"
     print(url)
     response = requests.get(url)
@@ -57,7 +73,7 @@ def get_pokemon_types(format: str = "dict") -> dict | list:
 
     
 if __name__ == '__main__':
-    # print(get_pokemon_names())
     # print(get_pokemon_info("pikachu"))
+    # print(get_pokemon_names())
     # print(get_pokemon_types())
     pass
